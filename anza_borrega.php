58adf3751ad6cb20bee2afaf25fc8743c0a66017
DirtbagDrew <andrewmyerhb@gmail.com>
2018-04-30 15:03:44 -0700

    main page style and individual project page

---
+++ anza_borrega.php
@@ -0,0 +1,55 @@
+#!/usr/local/php5/bin/php-cgi
+<?php
+?>
+<!DOCTYPE html>
+<html lang ="en">
+<head>
+	<meta charset="utf-8">
+	<title>LeviThompsonMedia</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link rel="stylesheet" href="project_style.css">
+	<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+</head>
+
+<body>
+	<header>
+		<div id="top_bar">
+			<img src="images/logo.png" alt="logo" class="logo">
+			<nav>
+				<ul>
+					<li><a href='index.php'>Home</a><li>
+					<li><a href='#'>About </a><li>
+					<li><a href='#'>Projects</a><li>
+					<li><a href='#'>Contact</a><li>
+				</ul>
+			</nav>
+		</div>
+	</header>
+	<div class="title_bar">
+		<h1> Anza Borrega</h1>
+	</div>
+	<img src="images/slideshow1.jpg" class="feat_pic">
+	<div class="feed">
+		
+		<div class="feat_proj">
+			<p>blah blah blah blah blah blah blah blahblah blah blah blahblah blah blah blahblah blah blah blahblah blah blah blahblah blah blah blahblah blah blah blahblah blah blah blahblah blah blah blahblah blah blah blahblah blah blah blahblah blah blah blahblah blah blah blahblah blah blah blahblah blah blah blahblah blah blah blahblah blah blah blahblah blah blah blahblah blah blah blahblah blah blah blahblah blah blah blahblah blah blah blahblah blah blah blahblah blah blah blahblah blah blah blahblah blah blah blahblah blah blah blahblah blah blah blahblah blah blah blah</p>
+		</div>
+		<div class="feat_proj">
+			<img src="images/slideshow1.jpg" class="other_pics">
+			<img src="images/slideshow1.jpg" class="other_pics">
+			<img src="images/slideshow1.jpg" class="other_pics">
+			<img src="images/slideshow1.jpg" class="other_pics">
+			<img src="images/slideshow1.jpg" class="other_pics">
+			<img src="images/slideshow1.jpg" class="other_pics">
+			<img src="images/slideshow1.jpg" class="other_pics">
+			<img src="images/slideshow1.jpg" class="other_pics">
+			<img src="images/slideshow1.jpg" class="other_pics">
+		</div>
+	</div>
+	<div class="footer">
+		<p>any questions please contact me at <a href='mailto:levithompsonmedia@gmail.com'>levithompsonmedia@gmail.com</a></p>
+	</div>
+	
+</body>
+
+</html>
