4f4503647f71c8099b85ead76b840e9bbe4d4d10
Mario Lopez <Mario.LopezHerdandez@student.csulb.edu>
2018-05-06 11:45:15 -0700

    changed database name

--- login/include/membersite_config.php
+++ login/include/membersite_config.php
@@ -4,20 +4,20 @@ require_once("./include/fg_membersite.php");
 $fgmembersite = new FGMembersite();
 
 //Provide your site name here
-$fgmembersite->SetWebsiteName('user11.com');
+$fgmembersite->SetWebsiteName('Levi Thompson Media');
 
 //Provide the email address where you want to get notifications
-$fgmembersite->SetAdminEmail('user11@user11.com');
+$fgmembersite->SetAdminEmail('mariolopez027@gmail.com');
 
 //Provide your database login details here:
 //hostname, user name, password, database name and table name
 //note that the script will create the table (for example, fgusers in this case)
 //by itself on submitting register.php for the first time
-$fgmembersite->InitDB(/*hostname*/'localhost',
-                      /*username*/'prasanth',
-                      /*password*/'p',
-                      /*database name*/'testdb',
-                      /*table name*/'fgusers3');
+$fgmembersite->InitDB(/*hostname*/'cecsdb01.coe.csulb.edu',
+                      /*username*/'cecs470o27',
+                      /*password*/'aix4ie',
+                      /*database name*/'cecs470sec01og06',
+                      /*table name*/'users');
 
 //For better security. Get a random string from this link: http://tinyurl.com/randstr
 // and put it here
