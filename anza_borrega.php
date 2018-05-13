3a60a5a78dc713894363ff3e33e4d070343ff589
DirtbagDrew <andrewmyerhb@gmail.com>
2018-05-13 12:09:24 -0700

    deletion

--- anza_borrega.php
+++ anza_borrega.php
@@ -1,12 +1,35 @@
 #!/usr/local/php5/bin/php-cgi
 <?php
-$imageFolder = 'images/';
+$id=$_GET["id"];
+$dbname = 'cecs470sec01og06';
+$dbuser = 'cecs470o33';
+$dbpass = 'ooz4qu';
+$dbhost = 'cecs-db01.coe.csulb.edu';
+$connect = mysql_connect($dbhost, $dbuser, $dbpass) or die("Unable to Connect to '$dbhost'");
+mysql_select_db($dbname) or die("Could not open the db '$dbname'");
+$connString = "mysql:host=cecs-db01.coe.csulb.edu;dbname=cecs470sec01og06";
+$pdo = new PDO($connString,$dbuser,$dbpass);
+// end database connection
+$result = mysql_query("SELECT img_path FROM images WHERE prj_name='$id'",$connect);
+$img_path = array();
+while ($row = mysql_fetch_array($result)) {
+    array_push($img_path, $row["img_path"]);
+}
+$result1 = mysql_query("SELECT prj_description from projects WHERE prj_name='$id'",$connect);
+$proj_desc = array();
+while ($row = mysql_fetch_array($result1)) {
+    array_push($proj_desc, $row["prj_description"]);
+}
+$connect = null;
+
+
+/*$imageFolder = 'images/';
 $imageTypes = '{*.jpg,*.JPG,*.jpeg,*.JPEG,*.png,*.PNG,*.gif,*.GIF}';
 $images = glob($imageFolder . $imageTypes, GLOB_BRACE);
 $textFolder='./';
 $textTypes='{*.txt,*.text}';
 $text=glob($textFolder . $textTypes, GLOB_BRACE);
-$file = file_get_contents("$text[0]");
+$file = file_get_contents("$text[0]");*/
 ?>
 <!DOCTYPE html>
 <html lang ="en">
@@ -33,18 +56,18 @@ $file = file_get_contents("$text[0]");
 		</div>
 	</header>
 	<div class="title_bar">
-		<h1> Anza Borrega</h1>
+		<h1> <?php echo $id; ?></h1>
 	</div>
 
-	<img src="<?php echo $images[0] ?>" class="feat_pic">
+	<img src="<?php echo $img_path[0] ?>" class="feat_pic">
 	<div class="feed">
 
 		<div class="feat_proj">
-			<p><?php echo $file?></p>
+			<p><?php echo $proj_desc[0]?></p>
 		</div>
 		<div class="feat_proj">
 			<?php
-				foreach($images as $image){
+				foreach($img_path as $image){
 					echo "<div class=\"other_pics\"><img src=\"$image\" class=\"feat_pic\"></div>";
 				}
 			?>
